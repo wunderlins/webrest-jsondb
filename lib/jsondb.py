d1ed7505f319ec44e1ba4c7d66e1d7bcffc98e69
wus <swunderlin@gmail.com>
2016-03-12 12:33:11 +0100

    simple set api without paths. added lock and io error handling code in example. implemented commit()

--- lib/jsondb.py
+++ lib/jsondb.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Interface
+simple json file access with exclusive locking during write
 
+Interface
 db = new jsonndb("file.json")
 
 data = db.get("a/b")
   -> throw ExceptionNotFound if element does not exist
 
-db.set("a/b", value)
-	-> create intermediate elements if they do not exist
-
-db.delete("a/b")
-  -> raise ExceptionNotFound if element does not exist
+db.set(jsondata)
+	-> this method will alway overwire all data
 
 db.commit()
 	- lock file 
@@ -36,18 +34,18 @@ class jsondb(object):
 	__dirty = False
 	__path  = None
 	
-	def __cleanpath(self, path):
-		if len(path) > 0 and path[-1] == "/":
-			path = path[0:-1]
-		return path		
-	
 	def __init__(self, path):
 		self.__path = path
 		f = open(path, 'rb')
 		self.__data = json.load(f)
 		f.close()
 	
-	def __exists(self, path):
+	def __cleanpath(self, path):
+		if len(path) > 0 and path[-1] == "/":
+			path = path[0:-1]
+		return path		
+	
+	def exists(self, path):
 		path = self.__cleanpath(path)
 	
 		# root node always exists
@@ -55,11 +53,16 @@ class jsondb(object):
 			return True
 		
 		# loop over all parts in path
+		#print path
 		apath = path.split('/')
 		node = self.__data
 		for e in apath:
-			if e not in node.keys():
+			try:
+				if e not in node.keys():
+					return False
+			except AttributeError, e:
 				return False
+				
 			node = node[e]
 		
 		return True
@@ -70,7 +73,7 @@ class jsondb(object):
 		if not path:
 			return self.__data
 		
-		if not self.__exists(path):
+		if not self.exists(path):
 			raise ExceptionNotFound("Element "+path+" not found in " + self.__path)
 		
 		# FIXME: two loops for lookup is inefficient
@@ -81,32 +84,24 @@ class jsondb(object):
 			node = node[e]
 		
 		return node
+	
+	def is_dirty(self):
+		return self.__dirty
+	
+	def set(self, data):
+		self.__dirty = True
+		self.__data = data
 		
-	def set(self, path, data):
-		path = self.__cleanpath(path)
-		
-		if not path:
-			self.__data = data
-			return
-
-		node = self.__data
-		if self.__exists(path):
-			apath = path.split('/')
-			for e in apath[0:-1]:
-				node = node[e]
-			node[apath[-1]] = data
-			return
-		
-		# TODO: ceate intermediate nodes
-		
-	def delete(self, path):
-		raise Exception("Not implemented")
-
 	def commit(self):
-		if self.__dirty:
+		if self.__dirty == False:
 			return
 		
-		raise Exception("Not implemented")
+		# import only on write, we expect most operations to be read operations
+		import portalocker
+		file = open(self.__path, 'wb')
+		portalocker.lock(file, portalocker.LOCK_EX)
+		json.dump(self.__data, file)
+		file.close()
 
 if __name__ == "__main__":
 	"""Testing """
@@ -119,14 +114,16 @@ if __name__ == "__main__":
 	fd.close()
 	
 	# open db
-	db = jsondb(f)
+	try:
+		db = jsondb(f)
+	except IOError, e:
+		print e
+		sys.exit(1)
 	
 	# check if an element exists
-	""" disable checks, existst renamed to __exists
 	assert db.exists("") == True
 	assert db.exists("a/b") == True
 	assert db.exists("a/e") == False
-	"""
 	
 	assert db.get("/") == {"a": {"b": 1, "c": 2, "d": [1,2,3]}}
 	assert db.get("") == {"a": {"b": 1, "c": 2, "d": [1,2,3]}}
@@ -134,9 +131,10 @@ if __name__ == "__main__":
 	assert db.get("a") == {"b": 1, "c": 2, "d": [1,2,3]}
 	assert db.get("a/") == {"b": 1, "c": 2, "d": [1,2,3]}
 	
-	assert db.set("a/b", 15) == None
-	assert db.get("") == {"a": {"b": 15, "c": 2, "d": [1,2,3]}}
-	assert db.set("", {"a": 12}) == None
+	assert db.set({"a": 12}) == None
 	assert db.get("") == {"a": 12}
+	try:
+		assert db.commit() == None
+	except LockException, e:
+		print "failed to write to file, locked exclusively"
 	
-
